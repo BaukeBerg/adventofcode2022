37b719463daed103c94309dd65e9906587538528
BaukeBerg <bauke.van.den.berg@gmail.com>
2022-12-11 08:02:52 +0100

    First part
    Very messy

---
+++ Assignment11.cpp
@@ -0,0 +1,171 @@
+#include "AdventClass.h"
+
+#include "FileUtility.h"
+#include "LimitFunctions.h"
+
+#include <set>
+
+#include "LimitFunctions.h"
+
+#include "ToStringConversion.h"
+#include <numeric>
+#include <StringHandler.h>
+
+class TestAssignment11 : public TAdventClass
+{
+
+};
+
+TEST_F(TestAssignment11, TestDayEleven)
+{
+  struct TItemInfo {
+    wxInt64 Level;
+    wxInt64 NextMonkey;
+  };
+
+  class TMonkey
+  {
+  public:
+    TMonkey(wxInt64 Index, TCVector<TCVector<TItemInfo>>* Items)
+    {
+      
+      this->Index = Index;
+      this->Items = Items;
+    }
+
+    void Parse(wxString Line)
+    {
+      Line = Line.Trim(false);
+      Line.Replace(":", ";");
+      if (Line.StartsWith("Start"))
+      {
+        auto Content = Split(Line, ";")[1];
+        Items->push_back(TCVector<TItemInfo>());
+        for (auto& Item : Split(Content, ","))
+        {
+          Items->at(Index).push_back({ StringToInt<wxInt64>(Item), Index });
+        }
+      }
+      else if (Line.StartsWith("Operation"))
+      {
+        ;
+
+      }
+      else if (Line.StartsWith("Test"))
+      {
+        Divider = StringToInt<wxInt64>(Line);
+      }
+      else if (Line.StartsWith("If true"))
+      {
+        TargetTrue = StringToInt < wxInt64>(Line);
+      }
+      else if (Line.StartsWith("If false"))
+      {
+        TargetFalse = StringToInt < wxInt64>(Line);
+      }
+    }
+    void OperateItems(void)
+    {
+      TCVector<TItemInfo> UpdatedWorryLevel;
+      for (auto& Item : Items->at(Index))
+      {
+        auto Value = Item.Level;
+        //if (Index != (Real ? 6 : 2))
+        {
+          Value = DoOperation(Value % Divider);          
+        }
+        Value = Value / 3;
+        auto Target = ((Value % Divider) == 0LL) ? TargetTrue : TargetFalse;
+        if (Value < 0)
+        {
+          TLogError("Overflow");
+        }
+        UpdatedWorryLevel.push_back({ Value, Target });
+        ++InspectionCycles;
+      }
+      Items->at(Index).clear();
+      for (auto& Item : UpdatedWorryLevel)
+      {
+        Items->at(Item.NextMonkey).push_back(Item);
+      }      
+    }
+
+    wxInt64 DoOperation(wxInt64 Value) {
+      switch (Index)
+      {
+      case 0: return Real ? (Value * 7LL) : (Value * 19LL);
+      case 1: return Real ? (Value * 11LL) : (Value + 6LL);
+      case 2: return Real ? (Value + 8LL) : (Value * Value);
+      case 3: return Real ? (Value + 7LL) : (Value + 3LL);
+      case 4: return (Value + 5LL);
+      case 5: return (Value + 4LL);
+      case 6: return (Value * Value);
+      case 7: return (Value + 3LL);
+      }
+    }
+
+    wxString AsString(void) const
+    {
+      wxString ReturnValue("Monkey ");
+      ReturnValue += ToString(Index) + ". Content: {";
+      for (auto& Item : Items->at(Index)) {
+        ReturnValue += IntToString<wxInt64>(Item.Level) + ", ";
+      }
+      ReturnValue.RemoveLast(2);
+      return ReturnValue + "}";
+    }
+    bool Real = false;
+    wxInt64 Divider;
+    wxString Operation;
+    TCVector<TCVector<TItemInfo>>* Items;
+    wxInt64 Index;
+    wxInt64 TargetTrue;
+    wxInt64 TargetFalse;
+    wxInt64 InspectionCycles = 0;
+  };
+
+  TCVector<TMonkey> Monkeys;
+  TCVector<TCVector<TItemInfo>> ItemList;
+  auto Input = ReadFileLines(MiniInput());
+  for (auto& Line : Input)
+  {
+    if (Line.StartsWith("Monkey"))
+    {
+      Monkeys.push_back(TMonkey(Monkeys.size(), &ItemList));
+    }
+    else
+    {
+      Monkeys.back().Parse(Line);
+    }
+  }
+
+  for (auto& Monkey : Monkeys)
+  {
+    RecordProperty(Monkey.AsString());
+  }
+
+  for (auto Iterator = 0; 1000 > Iterator; ++Iterator)
+  {
+    for (auto& Monkey : Monkeys)
+    {
+      Monkey.OperateItems();
+    }
+    /*for (auto& Monkey : Monkeys)
+    {
+      RecordProperty(Monkey.AsString());
+    }*/
+
+  }
+  TCVector<wxInt64> Cycles;
+  TCVector<wxInt64> Expected = { 5204,4792,199,5192 };
+  for (auto& Monkey : Monkeys)
+  {
+    Cycles.push_back(RecordProperty("Cycles: ", Monkey.InspectionCycles));
+    //EXPECT_EQ(Expected.at(Monkeys.IndexOf(Monkey)), Cycles.back());
+   // RecordProperty("Content: ", Monkey.AsString());
+
+  }
+  Cycles.Sort();
+  auto Value = Cycles.at(Cycles.size()-1) * Cycles.at(Cycles.size()-2);
+  EXPECT_EQ(2713310158, RecordProperty("CycleValue: ", Value));
+}
