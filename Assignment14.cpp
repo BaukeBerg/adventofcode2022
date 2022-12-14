c0e3c4d2dc469f7b403d842287424aaf43a24349
BaukeBerg <bauke.van.den.berg@gmail.com>
2022-12-14 07:22:34 +0100

    Update Assignment14.cpp
    Small visu for the cave

--- Assignment14.cpp
+++ Assignment14.cpp
@@ -34,72 +34,82 @@ public:
 TEST_F(TestAssignment14, TestSamples)
 {
   TGrid Grid;
-  
+
   auto Point = [](wxString Input)
   {
     auto Xy = Split(Input, ",");
     return wxPoint(StringToInt<wxInt32>(Xy[0]), StringToInt<wxInt32>(Xy[1]));
   };
-  
-  Grid.resize(1000, TCVector<wxInt32>(1000, wxNOT_FOUND));
+
+  auto MaxY = 0;
+
+  Grid.resize(3000, TCVector<wxInt32>(3000, wxNOT_FOUND));
   for (auto& Line : ReadFileLines(RealInput()))
   {
 
     auto Tokens = Split(Line, ">");
 
-    for (auto Index = 1; Index < Tokens.size() ; ++Index)
+    for (auto Index = 1; Index < Tokens.size(); ++Index)
     {
       auto To = Point(Tokens.at(Index));
-      auto From = Point(Tokens.at(Index-1));
+      auto From = Point(Tokens.at(Index - 1));
+      From.x += 1000;
+      To.x += 1000;
       Grid.Set(From, 1);
       Grid.Set(To, 1);
+      MaxY = Maximum(From.y, MaxY);
+      MaxY = Maximum(To.y, MaxY);
+
       if (To.x == From.x)
       {
         auto Increment = From.y < To.y ? 1 : -1;
         for (auto Iterator = From.y; Iterator != To.y; Iterator += Increment)
         {
           Grid.Set({ From.x, Iterator }, 1);
-        }        
+        }
       }
       else if (From.y == To.y)
       {
         auto Increment = From.x < To.x ? 1 : -1;
         for (auto Iterator = From.x; Iterator != To.x; Iterator += Increment)
         {
-          Grid.Set({ Iterator, From.y}, 1);
+          Grid.Set({ Iterator, From.y }, 1);
         }
       }
-
     }
   }
-
-  RecordProperty("Grid Filled");
-  for (auto& Item : Grid) {
-    wxString Line = "";
-    for (auto& Char : Item) {
-      Line += Char == wxNOT_FOUND ? '.' : '#' ;
-    }
-    RecordProperty(Line);
+  for (auto Iterator = 0; Iterator < 3000; ++Iterator) {
+    Grid.Set({ Iterator, MaxY + 2 }, 1);
   }
-  
+
+  //RecordProperty("Grid Filled");
+  //for (auto& Item : Grid) {
+  //  wxString Line = "";
+  //  for (auto& Char : Item) {
+  //    Line += Char == wxNOT_FOUND ? '.' : '#' ;
+  //  }
+  //  RecordProperty(Line);
+  //}
+  wxInt32 SandDrops = 0, TotalSand = 0;
   bool SandDropped = false;
   for (auto Iterator = 0; !SandDropped; ++Iterator)
   {
-    wxPoint Start(500, 0);
-    while (true) 
+    wxPoint Start(1500, 0);
+    while (true)
     {
       Start += wxPoint(0, 1);
-      if (Start.y >= 1000) 
+      if (Start.y >= 1500)
       {
+        SandDrops = Iterator;
         RecordProperty(wxString("Off the grid after: ") + Iterator + "Drops of sand");
         SandDropped = true;
         break;
       }
-      else if (Grid.Value(Start) != wxNOT_FOUND) 
+      else if (Grid.Value(Start) != wxNOT_FOUND)
       {
-        if (Grid.Value(Start + wxPoint(-1, 0)) == wxNOT_FOUND) 
+        if (Grid.Value(Start + wxPoint(-1, 0)) == wxNOT_FOUND)
         {
-          Start += wxPoint(-1,0);
+          Start += wxPoint(-1, 0);
         }
         else if (Grid.Value(Start + wxPoint(1, 0)) == wxNOT_FOUND)
         {
@@ -107,7 +117,14 @@ TEST_F(TestAssignment14, TestSamples)
         }
         else
         {
-          Grid.Set(Start + wxPoint(0, -1), 0); // All sand / rock => Block right above              
+          Grid.Set(Start + wxPoint(0, -1), 0); // All sand / rock => Block right above    
+          if (Grid.Value({ 1500,0 }) != wxNOT_FOUND)
+          {
+            TotalSand = Iterator++;
+            RecordProperty(wxString("Grid full after: ") + TotalSand + "Drops of sand");
+            SandDropped = true;
+            break;
+          }
           break;
         }
       }
@@ -116,5 +133,33 @@ TEST_F(TestAssignment14, TestSamples)
 
   }
 
+  EXPECT_EQ(888, SandDrops);
+  EXPECT_EQ(26461, TotalSand);
+
+  for (auto Index = 0; Index <= MaxY; ++Index) {
+    wxString Line = "";
+    for (auto& Char : Grid[Index])
+    {
+      switch (Char)
+      {
+      case -1:
+      {
+        Line += '_';
+        break;
+      }
+      case 0:
+      {
+        Line += '0';
+        break;
+      }
+      case 1:
+      {
+        Line += '#';
+        break;
+      }
+      }
+    }
+    RecordProperty(Line);
+  }
 }
 
