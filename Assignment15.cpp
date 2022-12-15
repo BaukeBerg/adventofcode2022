dd17b120aae07a3543d43166b8d9cbe24528e0e4
BaukeBerg <bauke.van.den.berg@gmail.com>
2022-12-15 08:08:44 +0100

    Update Assignment15.cpp
    Scan locations of bounding box of diamond against other areas to find the unique output

--- Assignment15.cpp
+++ Assignment15.cpp
@@ -5,7 +5,6 @@
 #include "StringFunctions.h"
 #include "StringHandler.h"
 
-
 class TestAssignment15 : public TAdventClass
 {
 public:
@@ -29,53 +28,157 @@ public:
     }
   };
 
+
+  struct TBeaconInfo {
+    wxPoint Scanner;
+    wxPoint Beacon;
+    wxInt32 Steps;
+  };
+
+  bool PointInDiamond(TBeaconInfo Info, wxPoint Pos)
+  {
+    auto Dist = Info.Scanner.y - Pos.y;
+    auto SizeOfDiamond = Info.Steps - Dist;
+    return InLimits(Info.Scanner.x-SizeOfDiamond, Pos.x, Info.Scanner.x+SizeOfDiamond);
+  }
+
+  bool OutsideAllDiamonds(wxPoint Pos) 
+  {
+    if (InLimits(0, Pos.x, 4000000)
+      && InLimits(0, Pos.y, 4000000))
+    {
+      bool UniquePos = true;
+      for (auto& Item : AllBeacons)
+      {
+        if(PointInDiamond(Item, Pos))
+        {          
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+
+  TCVector<TBeaconInfo> AllBeacons;
 };
 
 TEST_F(TestAssignment15, TestSamples)
 {
-
   auto Point = [&](const wxString& Info) {
     auto Token = Split(Info, ",");
     return wxPoint(StringToInt<wxInt32>(Token[0]), StringToInt<wxInt32>(Token[1]));
   };
 
-  auto LineToCheck = 2000000;
-  wxInt32 MinY = wxINT32_MAX;
-  wxInt32 MaxY = wxINT32_MIN;
+  auto i = 0;
   for (auto& Line : ReadFileLines(RealInput()))
   {
     auto Info = Split(Line, "b");
     auto Scanner = Point(Info[0]);
     auto Beacon = Point(Info[1]);
-    auto Steps = abs(Beacon.x - Scanner.x) + abs(Beacon.y - Scanner.y) +1;
-    auto LineSize = 1;
-    auto Pos = Scanner + wxPoint(0, -Steps);
-    for (auto Iterator = 0; Iterator < (2 * Steps + 1); ++Iterator)
-    {
-      
-      if (Pos.y < Scanner.y)
-      {
-        LineSize++;
-      }
-      if (Pos.y > Scanner.y)
-      {
-        LineSize--;
-      }
-      
-      if (Pos.y == 2000000)
-      {
-        MinY = Minimum(MinY, Pos.x);
-        MaxY = Maximum(MaxY, Pos.x);
-        
-        
-      }
-      Pos += wxPoint(0, 1);
-    }
-
+    auto Steps = abs(Scanner.x - Beacon.x) + abs(Scanner.y - Beacon.y);
+    AllBeacons.push_back({ Scanner, Beacon, Steps });
   }
-  auto Delta = MaxY - MinY;
-  RecordProperty("Blocked cells: ", Delta);
-  EXPECT_EQ(4985193, Delta);
+  
+  for (auto& Item : AllBeacons)
+  {
+    for (auto Iterator = 0; Iterator < Item.Steps; ++Iterator)
+    {
+      TCVector<wxPoint> Points = { 
+        Item.Scanner + wxPoint(Item.Steps - Iterator+1, Iterator),
+        Item.Scanner + wxPoint(-Item.Steps + Iterator-1, -Iterator),
+        Item.Scanner + wxPoint(Iterator+1, Item.Steps - Iterator),
+        Item.Scanner + wxPoint(-Iterator-1, -Item.Steps + Iterator) };
 
+      for (auto& Point : Points)
+      {
+        if (OutsideAllDiamonds(Point))
+        {
+          EXPECT_EQ(11583882601918LL, RecordProperty("Beacon value: " + wxInt64(Point.x) * 4000000LL + wxInt64(Point.y)));
+        }
+      }
+    }
+  }
+  return;
 }
-
+//
+//void CheckPart1(void)
+//{
+//  auto LineToCheck = 2000000;
+//  wxInt32 MinY = wxINT32_MAX;
+//  wxInt32 MaxY = wxINT32_MIN;
+//
+//  struct TInfo {
+//    wxPoint Start;
+//    wxInt32 Steps;
+//    bool I
+//  };
+//
+//  TCVector<TInfo> AllSteps;
+//
+//  wxInt32 DistressFrequency = 0;
+//
+//
+//    auto Steps = abs(Beacon.x - Scanner.x) + abs(Beacon.y - Scanner.y) + 1;
+//    AllSteps.push_back({ Scanner, Steps });
+//  }
+//
+//  for (auto& Item : AllSteps)
+//  {
+//     TCVector<wxPoint> Points = { Item.Start + wxPoint(0, Item.Steps), Item.Start + wxPoint(Item.Steps, 0), Item.Start + wxPoint(-Item.Steps, 0), Item.Start + wxPoint(0, -Item.Steps) };
+//     /*for (auto& Pos : Points)
+//     {
+//       if (!InLimits(0, Pos.x, 4000000)
+//         || !InLimits(0, Pos.y, 4000000))
+//       {
+//         continue;
+//       }
+//       RecordProperty("Beacon frequency: ", (4000000 * Pos.x) + Pos.y);
+//
+//     }
+//
+//
+//   }*/
+//   /*
+//   for(auto Item : AllSteps)
+//   {
+//     auto LineSize = 1;
+//     auto Pos = Scanner + wxPoint(0, -Steps);
+//
+//
+//     for (auto Iterator = 0; Iterator < (2 * Steps + 1); ++Iterator)
+//     {
+//
+//       if (Pos.y <= Scanner.y)
+//       {
+//         LineSize++;
+//       }
+//       if (Pos.y > Scanner.y)
+//       {
+//         LineSize--;
+//       }
+//
+//       if (Pos.y == 2000000)
+//       {
+//         MinY = Minimum(MinY, Pos.x);
+//         MaxY = Maximum(MaxY, Pos.x);
+//       }
+//       Pos += wxPoint(0, 1);
+//     }
+//
+//
+//
+//
+//   }
+//   a
+//
+//   uto Delta = MaxY - MinY;
+//   */
+//    RecordProperty("Blocked cells: ", Delta);
+//    EXPECT_EQ(4985193, Delta);
+//  }
+//  EXPECT_EQ(11583882601918, DistressFrequency);
+//
+//}
+//
