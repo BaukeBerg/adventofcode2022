b19a8e4250dd6322b08a3550e99c0f0685fd3778
BaukeBerg <bauke.van.den.berg@gmail.com>
2022-12-18 18:03:36 +0100

    Update Assignment18.cpp
    Flooding the sphere and couting where water touches the edges.

--- Assignment18.cpp
+++ Assignment18.cpp
@@ -34,6 +34,46 @@ public:
     {
       return x == Other.x && y == Other.y && z == Other.z;
     }
+
+    wxInt32 Max(void) const
+    {
+      return std::max({ x,y,z });
+    }
+  };
+
+  class TCube : public TCVector<TCVector<TCVector<wxInt32>>>
+  {
+  public:
+    void Set(TBlock Info, wxInt32 Value)
+    {
+      at(Info.x)[Info.y][Info.z] = Value;
+    }
+
+    wxInt32 At(TBlock Info)
+    {
+      return at(Info.x)[Info.y][Info.z];
+    }
+
+    bool Water(TBlock Point)
+    {
+      return 2 == At(Point);
+    }
+
+    bool Stone(TBlock Point)
+    {
+      return 1 == At(Point);
+    }
+
+    bool Valid(TBlock Point) const
+    {
+      wxInt32 Max = size() - 1;
+      return InLimits(0, Point.x, Max)
+        && InLimits(0, Point.y, Max)
+        && InLimits(0, Point.z, Max);
+
+    }
+
+
   };
 
 
@@ -42,27 +82,36 @@ public:
 TEST_F(TestAssignment18, TestDayOne)
 {
   TCVector<TBlock> Blocks;
+  auto MaxSize = 0;
   for (auto& Line : ReadFileLines(RealInput()))
   {
     Blocks.push_back(Line);
+    MaxSize = Maximum(MaxSize, Blocks.back().Max());
   }
+
+  auto OtherBlocks = [&](TBlock ThisBlock)
+  {
+    return TCVector<TBlock>({
+      TBlock({ ThisBlock.x - 1, ThisBlock.y, ThisBlock.z }),
+      TBlock({ ThisBlock.x + 1, ThisBlock.y, ThisBlock.z }),
+      TBlock({ ThisBlock.x, ThisBlock.y - 1, ThisBlock.z }),
+      TBlock({ ThisBlock.x, ThisBlock.y + 1, ThisBlock.z }),
+      TBlock({ ThisBlock.x, ThisBlock.y, ThisBlock.z - 1 }),
+      TBlock({ ThisBlock.x, ThisBlock.y, ThisBlock.z + 1 })
+      });
+  };
+
+  RecordProperty("Greatest dimension: ", MaxSize);
 
   for (auto Iterator = 0; Iterator < Blocks.size(); ++Iterator)
   {
     auto ThisBlock = &Blocks.at(Iterator);
-    TCVector<TBlock> OtherBlocks({
-      TBlock({ThisBlock->x - 1, ThisBlock->y, ThisBlock->z}),
-      TBlock({ThisBlock->x + 1, ThisBlock->y, ThisBlock->z}),
-      TBlock({ThisBlock->x, ThisBlock->y - 1, ThisBlock->z}),
-      TBlock({ThisBlock->x, ThisBlock->y + 1, ThisBlock->z}),
-      TBlock({ThisBlock->x, ThisBlock->y, ThisBlock->z - 1}),
-      TBlock({ThisBlock->x, ThisBlock->y, ThisBlock->z + 1})
-      });
 
-    for (auto& Block : OtherBlocks)
+
+    for (auto& Block : OtherBlocks(*ThisBlock))
     {
       for (auto Index = 0; Index < Blocks.size(); ++Index)
-      {        
+      {
         if (Blocks.at(Index) == Block)
         {
           ThisBlock->VisibleSides--;
@@ -70,12 +119,66 @@ TEST_F(TestAssignment18, TestDayOne)
       }
     }
   }
-  auto TotalSides = 0;
+  auto TotalSides = [&]() {
+    auto Total = 0;
+    for (auto& Block : Blocks)
+    {
+      Total += Block.VisibleSides;
+    }
+    return Total;
+  };
+
+  TCube Cube;
+  ++MaxSize;
+  Cube.resize(MaxSize, TCVector<TCVector<wxInt32>>(MaxSize, TCVector<wxInt32>(MaxSize, 0)));
   for (auto& Block : Blocks)
   {
-    TotalSides += Block.VisibleSides;
+    Cube.Set(Block, 1);
   }
-  EXPECT_EQ(64, RecordProperty("Open sides: ", TotalSides));
 
+  auto Total = TotalSides();
+  EXPECT_EQ(4310, RecordProperty("Open sides: ", Total));
 
+  TCVector<TBlock> Positions = { {0,0,0} };
+  TCVector<TBlock> NextSteps;
+  while (Positions.size() > 0)
+  {
+    NextSteps.clear();
+    for (auto& Item : Positions)
+    {
+      if (Cube.Valid(Item) && 0 == Cube.At(Item))
+      {
+        Cube.Set(Item, 2);
+        NextSteps += OtherBlocks(Item);
+      }
+    }
+    Positions = NextSteps;
+  }
+
+  wxString TotalCube = "";
+  auto TotalPoints = 0;
+  for (auto x = 0; x < MaxSize; ++x)
+  {
+    for (auto y = 0; y < MaxSize; ++y)
+    {
+      for (auto z = 0; z < MaxSize; ++z)
+      {
+        TBlock This({ x,y,z });
+        for (auto& Item : OtherBlocks(This))
+        {
+          if (Cube.Stone(This) && (!Cube.Valid(Item) || (Cube.Water(Item))))
+          {
+            ++TotalPoints;
+          }
+        }
+        TotalCube += Cube.Stone(This) ? "#" : Cube.Water(This) ? "0" : "_";
+      }
+      TotalCube += "\n";
+    }
+    TotalCube += wxString::Format("\n\n Row %i:\n\n", x);
+  }
+
+  RecordProperty(TotalCube);
+
+  EXPECT_EQ(2466, RecordProperty("Without airpockets: ", (TotalPoints)));
 }
